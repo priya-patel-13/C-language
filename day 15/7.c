9c4c7e6314d81f482044d4b9936d924c00efcceb
priya-patel-13 <ppiyu4487@gmail.com>
2024-03-21 15:36:02 +0530

    ptrn

---
+++ day 15/7.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main()
+{
+	for(int i=65;i<=69;i++)
+	{
+		for(int j=i;j>=65;j--)
+		{
+			printf("%c",j);
+		}
+		printf("\n");
+	}
+}
---
+++ day 15/8.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main()
+{
+	int num;
+
+	for(int i=5;i>=1;i--)
+	{
+		for(int j=i;j<=5;j++)
+		{
+			printf("%3d",num);
+		    num++;
+		}
+		printf("\n");
+
+
+	}
+}
