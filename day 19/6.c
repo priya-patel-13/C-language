9a19288b4e77602388e894aa40c70fe2de06eaf2
priya-patel-13 <ppiyu4487@gmail.com>
2024-03-28 15:04:59 +0530

    exam5

---
+++ day 19/6.c
@@ -0,0 +1,17 @@
+#include <stdio.h> 
+void main() 
+{ 
+    
+    for (int i = 0; i < 6; i++)
+    { 
+        for (int j = 0; j < 6; j++) { 
+            if (i > 0 && i < 6 - 1 && j > 0 && j < 6 - 1) { 
+                printf("  "); 
+            } 
+            else { 
+                printf("* "); 
+            } 
+        } 
+        printf("\n"); 
+    } 
+}
