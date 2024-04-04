66e5343707e4de063a0431e7b1521116e6430d2f
priya-patel-13 <ppiyu4487@gmail.com>
2024-04-04 14:39:45 +0530

    day24

---
+++ day 24/1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main(){
+    char val[]="HELLO WORLD";
+
+    for(int i=0; val[i] != NULL; i++)
+    {
+        if(val[i]>='A' && val[i]<='Z')
+        {
+            val[i] += 32;
+        }
+
+        printf("%c",val[i]);
+    }
+}
