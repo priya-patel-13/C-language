9de4ee60cba12d9bc799084761c40ac3f298c2b2
priya-patel-13 <ppiyu4487@gmail.com>
2024-04-05 16:03:33 +0530

    day 25

--- day25/7.c
+++ day25/7.c
@@ -1,7 +1,27 @@
-#include<stdio.h>
-main(){
+// Question 6:
+// Write a C program to take two strings as input from the user and compare if they are equal. If they are equal, print "Strings are equal," else print "No, they are not equal."
 
-char n1[]="PP";//80
-char n2[]="pp";//112
-printf("%d",strcmp(n1,n2));
+
+#include <stdio.h>
+#include<string.h>
+
+main() {
+    char str1[100], str2[100]; 
+	
+	printf("enter first:");
+	gets(str1);
+	printf("enter secound:");
+	gets(str2);
+	
+	printf("%d",strcmp(str1,str2));
+	
+	int a;
+	a=strcmp(str1,str2);
+	if(a==0)
+	
+		printf("string equal");
+	
+	else
+	printf("not equal...");
+    
 }
