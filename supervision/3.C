2a71f5f0a8c334556e67428a7e62b9679a7f21a7
priya-patel-13 <ppiyu4487@gmail.com>
2024-04-17 15:12:47 +0530

    superivison

---
+++ supervision/3.C
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+enum Days
+{
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday
+};
+
+int main()
+{
+    printf("Index values of weekdays:\n");
+    printf("Monday: %d\n", Monday);
+    printf("Tuesday: %d\n", Tuesday);
+    printf("Wednesday: %d\n", Wednesday);
+    printf("Thursday: %d\n", Thursday);
+    printf("Friday: %d\n", Friday);
+
+}
